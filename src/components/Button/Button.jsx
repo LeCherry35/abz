8ddafddfc088a27cb975b644b14083f196a143a1
Cherry <lenyamm35@gmail.com>
2023-12-13 15:15:29 +0200

    header, button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ name = "", onClick, disabled = false }) => {
+	Button.propTypes = {
+		name: PropTypes.string,
+		onClick: PropTypes.func,
+		disabled: PropTypes.bool,
+	};
+	return (
+		<button className="button" onClick={onClick} disabled={disabled}>
+			{name}
+		</button>
+	);
+};
+export default Button;
