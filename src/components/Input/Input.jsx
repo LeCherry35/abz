ad29ee83479bbc858f8a2db9c8fa93794aa4ef86
Cherry <lenyamm35@gmail.com>
2023-12-13 18:59:24 +0200

    input, radio input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Input = ({ placeholder, value, onChange, error }) => {
+  
+	Input.propTypes = {
+		placeholder: PropTypes.string,
+		value: PropTypes.string,
+		onChange: PropTypes.func,
+		error: PropTypes.string,
+	};
+	return (
+		<div className="text-input-container">
+			{value && <div className={error ? "text-input-container__input-name text-input-container__input-name_warning" : "text-input-container__input-name" }>{placeholder}</div>}
+			<input className={error ? " text-input-container__input text-input-container__input_warning" : "text-input-container__input" } type='text' placeholder={placeholder} value={value} onChange={onChange}/>
+			<div  className="text-input-container__warning-text">{error}</div>
+		</div>
+	);
+};
+
+export default Input;
---
+++ src/components/RadioInput/RadioInput.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const RadioInput = ({options=[], setSelected}) => {
+
+	RadioInput.propTypes = {
+		options: PropTypes.array,
+		setSelected: PropTypes.func,
+	};
+	return (
+		<div className="radio-inputs-container">
+			{options?.map((option, id) => {
+				return (
+					<label htmlFor={"input_" + id} className="radio-inputs-container__radio-label" key={option.id}>
+						<input 
+							className="radio-inputs-container__radio-input" 
+							id={"input_" + id} 
+							type="radio" 
+							value={option.id} 
+							name="radioInputs"
+							onClick={() => setSelected(option.id)}
+						/>
+						{option.name}
+					</label>
+				);
+			})}
+		</div>
+	);
+};
+
+export default RadioInput;
