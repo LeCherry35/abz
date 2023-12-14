67920570252844a679fb2e52aa628eb8a3af19f6
Cherry <lenyamm35@gmail.com>
2023-12-14 14:13:26 +0200

    header fixed

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Banner />
-			<ProfilesList />
-			<RegistrationForm />
+			<div className="App__content">
+				<Banner />
+				<ProfilesList />
+				<RegistrationForm />
+			</div>
 		</div>
 	);
 }
